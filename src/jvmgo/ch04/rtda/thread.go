0b446ba361cb83dab2292e75d0d243e0e755b2c0
zhangli <singlone@163.com>
2019-03-30 16:13:24 +0800

    ch04

---
+++ src/jvmgo/ch04/rtda/thread.go
@@ -0,0 +1,36 @@
+package rtda
+
+type Thread struct {
+	pc int //pc寄存器
+	stack *Stack //java虚拟机栈
+}
+
+
+/*
+JVM
+  Thread
+    pc
+    Stack
+      Frame
+        LocalVars
+        OperandStack
+*/
+
+func NewThread() *Thread {
+	return &Thread{stack:newStack(1024)}
+}
+
+//入栈
+func (t *Thread) PushFrame(frame *Frame) {
+	t.stack.push(frame)
+}
+
+//出栈
+func (t *Thread) PopFrame() *Frame {
+	return t.stack.pop()
+}
+
+//当前帧
+func (t *Thread) CurrentFrame() *Frame {
+	return t.stack.top()
+}
